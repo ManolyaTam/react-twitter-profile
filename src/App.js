b867af4b33cea0eac7e8f2775bac4a8dc0c3a6dd
Manolya Tamimi <tamimimanolya@gmail.com>
2023-09-18 20:17:46 +0300

    rough borders and main design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        logo here
+        <ul>
+          <li>Home</li>
+          <li>Explore</li>
+          <li>Notifications</li>
+          <li>Messages</li>
+          <li>Lists</li>
+          <li>Bookmarks</li>
+          <li>Communities</li>
+          <li>Profile</li>
+          <li>More</li>
+        </ul>
+        <button>Post</button>
+      </header>
+      <main>main</main>
     </div>
   );
 }
