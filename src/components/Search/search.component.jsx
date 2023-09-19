d5274748afd2ff0f2c8ceb914427e6a646056869
Manolya Tamimi <tamimimanolya@gmail.com>
2023-09-19 21:00:19 +0300

    fixing search style

--- src/components/Search/search.component.jsx
+++ src/components/Search/search.component.jsx
@@ -1,12 +1,12 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './search.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Search = () => {
   return (
     <div className="search">
       <FontAwesomeIcon className="search-magnifier" icon={faMagnifyingGlass} />
-      <input type="search" />
+      <input type="search" placeholder='Search'/>
     </div>
   );
 };
